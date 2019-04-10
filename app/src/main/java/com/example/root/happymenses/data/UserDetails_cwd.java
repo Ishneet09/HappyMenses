801d8f142d63c52a943aa945a834894d40eee7cc
Ishneet09 <43916654+Ishneet09@users.noreply.github.com>
2019-04-10 15:07:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/root/happymenses/data/UserDetails_cwd.java
@@ -0,0 +1,7 @@
+package com.example.root.happymenses.data;
+
+public class UserDetails_cwd {
+    public static String username = "";
+    public static String password = "";
+    public static String chatWith = "";
+}
